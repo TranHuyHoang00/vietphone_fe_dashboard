b64b0a7bdfa60923d4206bff563d77e5abd3a03e
Hoang <tranhuyhoang23022001@gmail.com>
2024-07-30 10:15:10 +0700

    update

--- src/apps/admins/managers/website/product/edit/product/elements/product_page.js
+++ src/apps/admins/managers/website/product/edit/product/elements/product_page.js
@@ -83,8 +83,11 @@ class product_page extends Component {
                                     <span>:</span>
                                 </div>
                                 <div className='w-2/3'>
-                                    <Input value={dataProductPage?.keywords} width={'100%'} disabled={!isEdit}
+                                    <textarea className='w-full border p-[5px]' rows={10} value={dataProductPage?.keywords} disabled={!isEdit}
                                         onChange={(event) => onChangeProductPage(event.target.value, 'keywords')} />
+
+                                    {/* <Input value={dataProductPage?.keywords} width={'100%'} disabled={!isEdit}
+                                        onChange={(event) => onChangeProductPage(event.target.value, 'keywords')} /> */}
                                 </div>
                             </div>
                         </div>
